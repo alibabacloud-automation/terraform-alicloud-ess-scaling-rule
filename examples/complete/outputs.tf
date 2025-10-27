c812b7e6bc71496c4ce4774b3e7ee2dcd0d26af7
shanye <shanye.xzq@alibaba-inc.com>
2025-10-27 11:21:48 +0800

    fix: improve the example/complete

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,4 @@
 output "this_ess_scaling_group_name" {
-  value = module.example.this_ess_scaling_group_name
+  description = "Name of the scaling group"
+  value       = module.example.this_ess_scaling_group_name
 }
